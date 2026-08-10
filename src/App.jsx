f08037e0acdf1b6062b0a199d3298aef4602f6e6
Brent Ogden <ogden87@gmail.com>
2026-08-09 20:43:54 -0600

    Code-split portfolio routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // src/App.jsx
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import ParallaxBackground from './components/ParallaxBackground'
 import Layout from './components/Layout'
 import Home from './pages/Home'
-import Projects from './pages/Projects'
-import ProjectDetail from './pages/ProjectDetail'
-import About from './pages/About'
 import ScrollToTop from './components/ScrollToTop'
-import ContactForm from './pages/ContactForm'
-import NotFound from './pages/NotFound'
-// import Projects2 from './pages/projects_new'
+
+const Projects = lazy(() => import('./pages/Projects'))
+const ProjectDetail = lazy(() => import('./pages/ProjectDetail'))
+const About = lazy(() => import('./pages/About'))
+const ContactForm = lazy(() => import('./pages/ContactForm'))
+const NotFound = lazy(() => import('./pages/NotFound'))
 
 export default function App() {
     return (
@@ -18,16 +18,17 @@ export default function App() {
             <ParallaxBackground />
             <ScrollToTop />
             <Layout>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/projects" element={<Projects />} />
-                    {/* <Route path="/projects_new" element={<Projects2 />} /> */}
-                    <Route path="/projects/:id" element={<ProjectDetail />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<ContactForm />} />
-                    <Route path="/contactform" element={<Navigate to="/contact" replace />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
+                <Suspense fallback={null}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/projects/:id" element={<ProjectDetail />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/contact" element={<ContactForm />} />
+                        <Route path="/contactform" element={<Navigate to="/contact" replace />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Suspense>
             </Layout>
         </div>
     )
